847d20e0708cf35c0a104cd374420307439e71f9
Janardhan Kumar <janardhankr2003@gmail.com>
2025-05-25 23:44:21 +0530

    📝 Added a Tkinter text editor with open, save, and new file features

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
+# import tkinter as tk
+# from tkinter import filedialog, messagebox
+
+# def new_file():
+#     text.delete(1.0,tk.END)
+
+# def open_file():
+#     file_path = filedialog.askopenfilename(defaultextension=".txt",filetypes=[("text files","*.txt")])
+#     if file_path:
+#         with open(file_path,'r') as file:
+#             text.delete(1.0, tk.END)
+#             text.insert(tk.END, file.read())
+
+# def save_file():
+#     file_path = filedialog.asksavesfilename(defaulttextextension='.txt',filetypes=[("text files","*.txt")])
+#     if file_path:
+#         with open(file_open,'w') as file:
+#             file.write(text.get(1.0,tk.END))
+#             messagebox.showinfo("info","file saves successfully")
+
+
+# root = tk.Tk()
+# root.title("simple text editor")
+# root.geometry("800x600")
+
+# menu=tk.Menu(root)
+# root.config(menu=menu)
+
+
+# file_name=tk.Menu(menu)
+# menu.add_cascade(label="file", menu=file_Menu)
+# file_menu.add_command(label="new", command=new_file)
+# file_menu.add_command(label="open", command=open_file)
+# file_menu.add_command(label="save", command=save_file)
+# file_menu.add_separator()
+# file_menu.add_command(label="exit", command=root.quit)
+
+
+# text=tk.text(root,wrap=tk.WORD, font=("Helvetica",12), fg="blue")
+# text.pack(expand=tk.YES, fill=tk.BOTH)
+
+# root.mainloop()
+
+
+
 import tkinter as tk
+from tkinter import filedialog, messagebox
+
+def new_file():
+    text.delete(1.0, tk.END)
+
+def open_file():
+    file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("text files", "*.txt")])
+    if file_path:
+        with open(file_path, 'r') as file:
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=[("text files", "*.txt")])
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))
+            messagebox.showinfo("info", "file saved successfully")
+
+root = tk.Tk()
+root.title("simple text editor")
+root.geometry("800x600")
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="file", menu=file_menu)
+file_menu.add_command(label="new", command=new_file)
+file_menu.add_command(label="open", command=open_file)
+file_menu.add_command(label="save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="exit", command=root.quit)
+
+text = tk.Text(root, wrap=tk.WORD, font=("Helvetica", 12), fg="blue")
+text.pack(expand=tk.YES, fill=tk.BOTH)
+
+root.mainloop()
